72ec8f6a621ea9b99356df8e50ba85e849d832dd
eshcol-shinigami <eshcolshinigami2@gmail.com>
2022-07-14 11:22:05 +0100

    .........

---
+++ scrit1_git.bat
@@ -0,0 +1,18 @@
+@REM je me positionne dans le repertoire qui contient tout les depots
+cd/d C:\Users\SHINIGAMI_ESHCOL\Desktop\DEPOT_LOCAL\test3
+@REM 
+git config --global user.name "eshcol-shinigami"
+@REM 
+git config --global user.email "eshcolshinigami2@gmail.com"
+@REM 
+git init
+@REM 
+git add *
+@REM 
+git commit -m "........."
+@REM 
+git branch -M main
+@REM 
+git remote add origin https://github.com/eshcol-shinigami/test3.git
+@REM 
+git push -u origin main   
